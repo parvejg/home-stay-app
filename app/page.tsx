9a0bfdc7848dd5b573ffebb80ceddda332cb2f7d
parvejg <parvezpg321@gmail.com>
2025-03-19 01:48:14 +0530

    add some category links

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <p>hello Home Stay</p>
+    <div className=" items-center  min-h-screen  pb-20  max-w-full font-[family-name:var(--font-geist-sans)]">
+      <div className="flex   p-4 pl-6 pr-6 overflow-x-auto shadow-black shadow-xs gap-5 text-lg justify-center items-center min-h-20 max-w-full mt-5 bg-gray-200">
+        <a href="" className="">Amazing pools</a>
+        <a href="">Icons</a>
+        <a href="">Farms</a>
+        <a href="">Rooms</a>
+        <a href="">Camping</a>
+        <a href="">Surfing</a>
+        <a href="">Amazing views</a>
+        <a href="">Historical homes</a>
+      </div>
     </div>
   );
 }
